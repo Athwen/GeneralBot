e130bc76c13509c09785a8d7177ce6b39f671410
Alex Mayorga <alexmayorga@knights.ucf.edu>
2021-03-13 20:19:57 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+require('dotenv').config();
+
+
+client.once('ready', ()=> {
+	console.log('Ready!');
+
+});
+
+client.login(process.env.TOKEN);
