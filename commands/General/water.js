21813090ae9c937374a18fba4d247dfc3a688f87
Alex Mayorga <alexmayorga@knights.ucf.edu>
2021-03-17 15:33:07 -0400

    Adds command usage and solves edge case for water command

--- commands/General/water.js
+++ commands/General/water.js
@@ -4,11 +4,17 @@ const usersPref = {};
 module.exports = {
 	name: 'water',
 	description: 'a water reminder',
+	args: true,
+	usage: '<minutes>',
 	execute(message, args) {
 		const minutesToMS = (args[0] * 60) * 1000;
 
 		if(args[0] < 10) {
 			message.reply('Enter an interval that is longer than 10 minutes');
+			return;
+		}
+		if(args[0] > 35700) {
+			message.reply('Enter an interval that wont break the bot pls');
 			return;
 		}
 
