6a53d490ddb2d1f3fee0a957672dc1ffa601714f
Alex Mayorga <alexmayorga@knights.ucf.edu>
2021-03-25 22:08:21 -0400

    Adds support for playlists

--- commands/General/play.js
+++ commands/General/play.js
@@ -3,12 +3,14 @@ const ytsearch = require('youtube-search');
 require('dotenv').config();
 const util = require('util');
 const axios = require('axios');
+const url = require('url');
 
 const EventEmitter = require('events');
 class MyEmitter extends EventEmitter {}
 const MusicEmitter = new MyEmitter();
 
 const servers = {};
+const YTLink = 'https://www.youtube.com/watch?v=';
 
 async function playSong(server) {
 	const dispatcher = server.connection.play(await ytdl(server.queue[0], { filter: 'audioonly' }), { type: 'opus', volume: 0.055 });
@@ -32,12 +34,17 @@ module.exports = {
 	MusicEmitter: MusicEmitter,
 	async execute(message, args) {
 		let link;
-		if(!args[0].startsWith('https')) {
+		if(!args[0].startsWith('https') && !args[0].includes('playlist')) {
 
 			const ytSearchPromise = util.promisify(ytsearch);
 
 			ytSearchPromise(args.join(), opts).then(results => {
+				if(!servers[message.guild.id]) {
+					servers[message.guild.id] = { queue: [] };
+
+				}
 				link = results[0].link;
+				servers[message.guild.id].queue.push(link);
 				console.log('link inside ytsearch:' + link);
 
 			}).catch(err => {
@@ -45,30 +52,54 @@ module.exports = {
 
 			});
 		} else if(args[0].includes('playlist')) {
-			const splitURL = args[0].split('list=');
-			const playlistID = splitURL[1];
+			if(!servers[message.guild.id]) {
+				servers[message.guild.id] = { queue: [] };
+			}
 
-			const getReq = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistID}&key=${process.env.YTKEY}`;
+			const currURL = new URL(args[0]);
+			const playlistID = currURL.searchParams.get('list');
+			console.log(playlistID);
+			let nextPageToken = '';
 
-			axios.get(getReq).then(response => {
-				console.log(response.data.items);
+			do{
+				const getReq = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&pageToken=${nextPageToken}&playlistId=${playlistID}&key=${process.env.YTKEY}`;
+				let i = 0;
+				await axios.get(getReq).then(response => {
+					console.log(response.data.items[0].snippet.resourceId.videoId);
+					for(i = 0; i < response.data.items.length; i++) {
+						console.log(response.data.items[i].snippet['resourceId'].videoId);
+						servers[message.guild.id].queue.push(`${YTLink}${response.data.items[i].snippet['resourceId'].videoId}`);
 
-			});
+					}
+
+					nextPageToken = response.data['nextPageToken'];
+
+				}).catch(error => {
+					console.log(error);
+
+				});
+
+			} while(nextPageToken != undefined);
+
+			console.log(servers[message.guild.id].queue);
 
 		} else {
-			link = args[0];
+			if(!servers[message.guild.id]) {
+				servers[message.guild.id] = { queue: [] };
+
+			}
+			servers[message.guild.id].queue.push(args[0]);
 		}
 
 
-		if(servers[message.guild.id]) {
-			servers[message.guild.id].queue.push(link);
-			console.log(servers[message.guild.id].queue);
-
-		}else {
+		if(servers[message.guild.id] == undefined) {
+			if(!message.member.voice) {
+				message.reply('You must be in a voice channel first!');
+				return;
+			}
 			const connection = await message.member.voice.channel.join();
-			servers[message.guild.id] = { queue: [] };
+
 			servers[message.guild.id].connection = connection;
-			servers[message.guild.id].queue.push(link);
 
 			MusicEmitter.on('nextSong', async () => {
 				if(!servers[message.guild.id]) return;
