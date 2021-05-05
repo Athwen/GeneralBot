d2bffefdeec6ddabe4fbe0e96c18e0a26b0975b1
Alex Mayorga <alexmayorga@knights.ucf.edu>
2021-05-05 19:18:49 -0400

    testing something

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const cors = require('cors');
 const express = require('express');
 const port = process.env.PORT || 5000;
+const path = require('path');
 
 require('dotenv').config();
 const prefix = process.env.PREFIX;
@@ -38,6 +39,28 @@ const app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(cors());
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('frontend/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+	});
+}
+
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+	);
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PATCH, DELETE, OPTIONS',
+	);
+	next();
+});
 
 const endPoints = fs.readdirSync('./api').filter(file => file.endsWith('.js'));
 for(const file of endPoints) {
