ee63926778b4647e56bd967d50b9ec9d4ca01d93
Alex Mayorga <alexmayorga@knights.ucf.edu>
2021-05-05 19:29:57 -0400

    test 2

--- index.js
+++ index.js
@@ -40,28 +40,6 @@ app.set('port', (process.env.PORT || 5000));
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-	// Set static folder
-	app.use(express.static('frontend/build'));
-
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-	});
-}
-
-app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-	);
-	res.setHeader(
-		'Access-Control-Allow-Methods',
-		'GET, POST, PATCH, DELETE, OPTIONS',
-	);
-	next();
-});
-
 const endPoints = fs.readdirSync('./api').filter(file => file.endsWith('.js'));
 for(const file of endPoints) {
 	const api = require(`./api/${file}`);
